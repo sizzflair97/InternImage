d0b55852e980ebf2feeb1ac53733f8bb3b5f136e
Sizz Flair <sizzflair97@gmail.com>
2023-09-06 16:29:56 +0900

    save inferences to csv

---
+++ segmentation/infer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+CUDA_VISIBLE_DEVICES=0
+
+python image_demo.py \
+  data/test_image \
+  configs/dacon/upernet_internimage_xl_512x1024_160k_dacon.py  \
+  work_dirs/upernet_internimage_xl_512x1024_160k_dacon/latest.pth \
+  --opacity 1.0
